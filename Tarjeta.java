da94ab5126799eeea9b71034919c9d301614b06c
Daredevil769 <david.nieto769@gmail.com>
2022-03-25 19:59:47 -0500

    Primer commit

---
+++ Tarjeta.java
@@ -0,0 +1,11 @@
+public class Tarjeta {
+    private int saldo;
+
+    public Tarjeta(int saldo){
+        this.saldo=saldo;
+    }
+    public Registro pagar(String servicio){
+
+
+    }
+}
---
+++ TarjetaPersonalizada.java
@@ -0,0 +1,2 @@
+public class TarjetaPersonalizada {
+}

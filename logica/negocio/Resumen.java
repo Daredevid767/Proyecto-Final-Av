f13097101e92151a3ab3b569c6adff8afe26c82c
David-nieto769 <75280820+Daredevid767@users.noreply.github.com>
2022-03-27 21:41:53 +0000

    Se complementó la clase resumen

--- logica/negocio/Resumen.java
+++ logica/negocio/Resumen.java
@@ -1,4 +1,28 @@
 package logica.negocio;
 
 public class Resumen {
+
+	private String nombreRuta;
+	private int gananciaTotal;
+	private int gananciaPorBus;
+	private int ganaciaPorTiempo;
+	private int pasajerosTotales;
+	private int pasajerosPorBus;
+	private int pasajerosPorTiempo;
+
+	public Resumen(String nombreRuta,int gananciaTotal,int gananciaPorBus,int gananciaPorTiempo,int pasajerosTotales,int pasajerosPorBus,int pasajerosPorTiempo){
+		
+		this.nombreRuta=nombreRuta;
+		this.gananciaTotal=gananciaTotal;
+		this.gananciaPorBus=gananciaPorBus;
+		this.gananciaPorTiempo=gananciaPorTiempo;
+		this.pasajerosTotales=pasajerosTotales;
+		this.pasajerosPorBus=pasajerosPorBus;
+		this.pasajerosPorTiempo=pasajerosPorTiempo;
+
+
+		
+	}
+	
+	
 }
